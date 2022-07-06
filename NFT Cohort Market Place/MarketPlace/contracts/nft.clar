c9ff38f91d6a880e53446701ec1c085118520620
eqan <eqan.ahmad123@gmail.com>
2022-07-06 12:41:49 +0500

    Updating NFT Market Place + Adding Test Case

--- NFT Cohort Market Place/MarketPlace/contracts/nft.clar
+++ NFT Cohort Market Place/MarketPlace/contracts/nft.clar
@@ -1,51 +1,59 @@
-(impl-trait .sip009-nft-traits.sip009-nft-trait)
-
-;; NFT MarketPlace
-;; MarketPlace smartcontract
+(impl-trait .sip009-nft-trait.sip009-nft-trait)
+;; nft
+;; <add a description here>
 
 ;; constants
 ;;
-(define-non-fungible-tokens cohort-art uint)
-(define-map art-data {id: uint} {price: uint, url: (optional (string-ascii 256))})
-(define-constant ERR_NOT OWNER (err u99))
+(define-non-fungible-token cohort-art uint)
+(define-map art-data { id: uint } {price: uint, url: (optional (string-ascii 256))})
+(define-constant ERR_NOT_OWNER (err u999))
 
 ;; data maps and vars
 ;;
 (define-data-var current-id uint u0)
 
-(define-read-only (get-last-token-id) 
-	(ok (var-get current-id))
+(define-read-only (get-last-token-id)
+    (ok (var-get current-id))
 )
 
 (define-read-only (get-owner (id uint))
-	(ok (nft-get-owner? cohort-art id))	
+
+    (ok (nft-get-owner? cohort-art id))
+
 )
 
-;; (some(typle)) -> {price: uint, url: {optional} (string ascii 256))} -> (string ascii 256)
-;; none
+;; (some (tuple)) ->  {price: uint, url: (optional (string-ascii 256))} -> (optional (string-ascii 256)) -> (ok (optional (string-ascii 256)))
+;; none 
+
 (define-read-only (get-token-uri (id uint))
-	(ok (get url(unwrap-panic (map-get? art-data {id: id}))))
+    (ok (get url (unwrap-panic (map-get? art-data { id: id }))))
 )
 
 (define-public (transfer (id uint) (sender principal) (reciever principal))
-	(begin
-		(asserts! (is-eq tx-sender sender) ERR_NOT_OWNER)
-		(nft-transfer? cohort-art id sender reciever)
-	)
+     (begin
+        (asserts! (is-eq tx-sender sender) ERR_NOT_OWNER)
+        (nft-transfer? cohort-art id sender reciever)
+    )
 )
 
-(define-public (mint! (price uint)url (optional (string-ascii 256)))
+(define-public (mint! (price uint) (url (optional (string-ascii 256))))
+
     (let
         (
             (token-id (+ (var-get current-id) u1))
         )
-        (try! (nft-mint? cohort-art token-id txp-sender))
-        (map-insert art-data {id: uint} {price: uint, url: url})
+        
+        (try! (nft-mint? cohort-art token-id tx-sender))
+
+        (map-insert art-data { id: token-id } {price: price, url: url})
+
         (var-set current-id token-id)
+
         (ok "Success")
     )
 )
 
-(define-public (burn! (price uint)))
-   (nft-burn? cohort-art id tx-sender)
+(define-public (burn! (id uint))
+
+    (nft-burn? cohort-art id tx-sender)
 )
--- NFT Cohort Market Place/MarketPlace/tests/nft_test.ts
+++ NFT Cohort Market Place/MarketPlace/tests/nft_test.ts
@@ -1,26 +1,124 @@
-
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.28.0/index.ts';
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.31.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that minting is working!",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+        const deployer = accounts.get("deployer")!;
+        const wallet2 = accounts.get("wallet_1")!;
+
+        let block = chain.mineBlock([
+           
+           Tx.contractCall("nft", "mint!", [types.uint(200), types.some(types.ascii("ummar.jpeg"))], deployer.address)
+          
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.height, 2);
+        
+        block.receipts[0].result.expectOk()
+        .expectAscii("Success")
+
+        
+        block.receipts[0].events.expectNonFungibleTokenMintEvent(types.uint(1), deployer.address, 
+        `${deployer.address}.nft`, "cohort-art")
+
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that owner is correct!",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+        const deployer = accounts.get("deployer")!;
+        const wallet2 = accounts.get("wallet_1")!;
+
+        let block = chain.mineBlock([
+           
+           Tx.contractCall("nft", "mint!", [types.uint(200), types.some(types.ascii("ummar.jpeg"))], deployer.address),
+           Tx.contractCall("nft", "get-owner", [types.uint(1)], deployer.address)
+          
+        ]);
+
+        assertEquals(block.receipts.length, 2);
+        assertEquals(block.height, 2);
+        
+        block.receipts[0].result.expectOk()
+        .expectAscii("Success")
+
+        const owner = block.receipts[1].result.expectOk().expectSome()
+
+        assertEquals(owner, deployer.address)
+        
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that transfer is working properly!",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+        const deployer = accounts.get("deployer")!;
+        const wallet2 = accounts.get("wallet_1")!;
+
+        let block = chain.mineBlock([
+           
+           Tx.contractCall("nft", "mint!", [types.uint(200), types.some(types.ascii("ummar.jpeg"))], deployer.address),
+           Tx.contractCall("nft", "transfer", [types.uint(1), types.principal(deployer.address), types.principal(wallet2.address)], deployer.address),
+           Tx.contractCall("nft", "get-owner", [types.uint(1)], deployer.address)
+          
+        ]);
+
+        assertEquals(block.receipts.length, 3);
+        assertEquals(block.height, 2);
+        
+        block.receipts[0].result.expectOk()
+        .expectAscii("Success")
+
+        block.receipts[1].events.expectNonFungibleTokenTransferEvent(
+            types.uint(1),
+            deployer.address, 
+            wallet2.address, 
+            `${deployer.address}.nft`,
+            "cohort-art"
+        )
+
+        const owner = block.receipts[2].result.expectOk().expectSome()
+
+        assertEquals(owner, wallet2.address)
+        
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that burn is working properly!",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+
+        const deployer = accounts.get("deployer")!;
+        const wallet2 = accounts.get("wallet_1")!;
+
+        let block = chain.mineBlock([
+           
+           Tx.contractCall("nft", "mint!", [types.uint(200), types.some(types.ascii("ummar.jpeg"))], deployer.address),
+           Tx.contractCall("nft", "burn!", [types.uint(1)], deployer.address),
+           Tx.contractCall("nft", "get-owner", [types.uint(1)], deployer.address)
+          
+        ]);
+
+        assertEquals(block.receipts.length, 3);
+        assertEquals(block.height, 2);
+        
+        block.receipts[0].result.expectOk()
+        .expectAscii("Success")
+
+        block.receipts[1].events.expectNonFungibleTokenBurnEvent(
+            types.uint(1),
+            deployer.address, 
+            `${deployer.address}.nft`,
+            "cohort-art"
+        )
+
+        block.receipts[2].result.expectOk().expectNone()
+        
     },
 });
